5dafe82dbe62529a2ac8749dc51c30afa173efc1
matihullahIT <matihurrehman.official@gmail.com>
2025-05-04 21:30:20 +0500

    Initialize project with Vite, React, and Tailwind CSS setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className='bg-blue-400 font-bold'>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero et repudiandae nemo reprehenderit corrupti eveniet soluta at, provident, sunt odit facilis iusto voluptatem quasi illum similique repellat architecto! Animi, eaque!
+       </div>
+    </>
+  )
+}
+
+export default App
