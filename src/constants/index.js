28e73363e9a6253f988985e6b4b6a214a7a467fc
matihullahIT <matihurrehman.official@gmail.com>
2025-05-10 15:01:47 +0500

    update src/constants/index.js

---
+++ src/constants/index.js
@@ -0,0 +1,83 @@
+export const Logo="LOGO"
+const  description="Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscin Lorem "
+const btn="Get a Quote"
+export const Hero = {
+    title: "Transforming Digital Brands Since 1898",
+    img: [
+        '/src/assets/images/bannerimg1.svg',
+        '/src/assets/images/bannerimg 2.svg',
+        '/src/assets/images/bannerimg 3.svg'
+    ],
+    btn: "Get a Quote",
+    ctabtn: "See Our Work"
+};
+export const CardSection=[
+    {
+        id:"0",
+        color:"FA0069",
+        title:"Graphic Design",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscin Lorem ",
+        btn:btn
+        
+    },
+    {
+        id:"1",
+        color:"002FD8",
+        title:"Web Development",
+        description:description,
+        btn:btn
+    }
+    ,
+    {
+        id:"2",
+        color:"04CA00",
+        title:"Visual Consultation",
+        description:description,
+        btn:btn
+    }
+    ,
+    {
+        id:"3",
+        color:"B900FA",
+        title:"Product Design",
+        description:description,
+        btn:btn
+    }
+]
+export const testimonials=[
+    {
+        id:"1",
+        btn:"Flowers Design",
+        img:"../assets/images/testimonial 1.svg"
+    },
+    {
+        id:"2",
+        btn:"Abstract Design",
+        img:"../assets/images/testimonial 2.svg"
+    }
+    ,
+    {
+        id:"3",
+        btn:"Creative Painting",
+        img:"../assets/images/testimonial 3.svg"
+    }
+    ,
+    {
+        id:"4",
+        btn:"Product Design",
+        img:"../assets/images/testimonial 4.svg"
+    }
+    ,
+    {
+        id:"5",
+        btn:"Creative Painting",
+        img:"../assets/images/testimonial 5.svg"
+    }
+]
+export const footer={
+    logo:"LOGO",
+    date:"1898-2022",
+    text:["Home","Works","About Us","Contact Us","Careers","Investors","Developers","Refferal Program",
+        "Privacy Policy","Cookie Policy","Terms and Conditions"
+    ]
+}
