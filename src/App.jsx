040d751cc43d612d3b57794472d31e64bf4b0f97
matihullahIT <matihurrehman.official@gmail.com>
2025-05-16 22:27:36 +0500

    this is first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
-// import Navbar from "./components/navbar"
-import HeroSection from "./components/HeroSection"
-import Cards from "./components/cards";
-import Testimonials from "./components/testimonails";
-import Contact_us from "./components/Contact us"
-import Footer from "./components/footer";
 import "./index.css"
 function App() {
   document.title="LOGO";
   return (
     <>
     <div>
-      {/* <Navbar/>k */}
-      <HeroSection/>
-      <Cards/>
-      <Testimonials/>
-      <Contact_us/>
-      <Footer/>
+      <h2 className="bg-blue-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis odio officia neque praesentium nihil adipisci necessitatibus est, voluptas tenetur totam sint magni autem laborum assumenda error illo, modi repudiandae eum consequuntur mollitia exercitationem atque vitae dolorum facilis. Nemo nostrum totam, dicta repellendus nihil vitae quae consectetur sequi, porro fugiat inventore nisi blanditiis vero nulla labore quasi rem accusamus sapiente. Voluptatibus animi aspernatur repudiandae. In architecto facilis veritatis exercitationem officia pariatur non perferendis modi consequuntur! Eligendi rem atque sed laborum voluptatem, maxime est amet excepturi vero tempora aliquam sapiente ratione veritatis ex sequi. Voluptatibus, vero ipsum. Nulla commodi deserunt incidunt at.</h2>
     </div>
     </>
   )
